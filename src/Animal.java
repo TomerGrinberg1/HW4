507c608815920d83b4dd458c0ebb15ebdd7d49ab
TomerGrinberg1 <grinbergtomer2@gmail.com>
2022-01-19 12:42:44 +0200

    Initial commit

---
+++ src/Animal.java
@@ -0,0 +1,9 @@
+/**
+ * An interface which represents a general animal.
+ *
+ * Note: Use this this interface and don't change it!
+ */
+public interface Animal {
+    void performAction();
+    void eat();
+}
